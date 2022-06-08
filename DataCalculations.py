827fd64d432ed7872d940e1ca6bef9560891f2db
emiel <e.nagel@student.utwente.nl>
2022-06-08 10:40:29 +0200

    Improved data calculations

--- DataCalculations.py
+++ DataCalculations.py
@@ -10,6 +10,14 @@ class DatCalc:
     def __init__(self):
         "Insert stuff"
         self.dataConnect()
+        self.curData = []
+        self.boots = "none"
+        self.legs = "none"
+        self.torso = "none"
+        self.helmet = "none"
+
+        self.gravityLimit = []
+        self.temperatureLimit = []
 
     def dataConnect(self):
         """
@@ -37,21 +45,60 @@ class DatCalc:
         self.planet = "Mercury"
         for i in range(0, len(self.rows)):
             if self.planet in self.rows[i]:
+                self.curData = self.rows[i]
                 print(self.rows[i])
 
     def survivalCalc(self):
         """
         This function calculates whether the astronaut will survive or not and in which areas
         """
+        '''
+        G-force = 1
+        Toxic = 2
+        Oxygen = 3
+        Surface Temperature = 4
+        Gas Giant = 5
+        '''
+        self.gravityCalc(self.curData[1])
+
+    def gravityCalc(self, data):
+        pass
+
+    def toxicityCalc(self, data):
+        pass
+
+    def oxygenCalc(self, data):
+        pass
+
+    def temperatureCalc(self, data):
+        pass
+
+    def gasGiantCalc(self, data):
+        pass
 
     def gearScoreCalc(self):
         """
-        This function calculates the values for physicalizing the planet data and passing to survivalcalc
+        This function calculates the properties of the gear from the astraut to pass to survivalcalc
         """
+
+        self.dataConnect()
+        self.curData = []
+        self.boots = "none"
+        self.legs = "none"
+        self.torso = "none"
+        self.helmet = "none"
+
+    def humanSurvivabilityCalc(self):
+        """
+        This function calculates the limits of human survivability
+        """
+        self.gravityLimit.append(0.85)
+        self.gravityLimit.append(1.15)
+
 
     def planetScoreCalc(self):
         """
-        This function calculates the properties of the gear from the astraut to pass to survivalcalc
+        This function calculates the values for physicalizing the planet data and passing to survivalcalc
         """
 
 
