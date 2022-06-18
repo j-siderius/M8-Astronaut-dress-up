44954260e260ebfe076e370df55e9b98a43da07f
Jannick <52700380+j-siderius@users.noreply.github.com>
2022-06-18 16:44:01 +0200

    updated heart rate visualisation to fix bugs + new library

--- heart_rate_test.py
+++ heart_rate_test.py
@@ -1,100 +1,89 @@
+import neurokit2 as nk  # Load the package
 import pygame
 from pygame.locals import *
-from scipy.misc import electrocardiogram
 import time
 
 
 def main():
-    # initialize pygame
-    pygame.init()
-    pygame.font.init()
-    screen = pygame.display.set_mode((400, 200))
-    screen_width, screen_height = pygame.display.get_surface().get_size()
+	pygame.init()
+	pygame.font.init()
 
-    # grab ECG data from scipy
-    ecg = electrocardiogram()  # 360Hz in mV, min: -3.485 max: 3.65
-    # screen = 400 x 200
-    # 400 points should translate to ±2 seconds
-    # 0 mV should be at y 100
-    index = 0
-    posX = 0  # posX on x-axis
+	# screen and variables
+	screen = pygame.display.set_mode((400, 200))
+	screen_width, screen_height = pygame.display.get_surface().get_size()
 
-    # initialize font and colours
-    font = pygame.font.SysFont('arial', 18)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    GREEN = (100, 255, 0)
+	font = pygame.font.SysFont('arial', 18)
+	WHITE = (255, 255, 255)
+	BLACK = (0, 0, 0)
+	GREEN = (100, 255, 0)
 
-    # timing variables
-    running = True
-    previous_time = previous_beat = time.perf_counter()
+	# change BPM here
+	bpm = 80
 
-    # calculate BPM speed
-    bpm_count = int(60)
-    _BPM_count = (bpm_count/60) * 200
+	if bpm > 2:
+		# ECG min = ±-0.5 max = ±1.5 >> draw 0 at screen_height * 0.75
+		ecg = nk.ecg_simulate(duration=16, sampling_rate=100, heart_rate=bpm)
+		initialY = int(screen_height * 0.7)
+	else:
+		# ded
+		ecg = [0.0] * screen_width
+		initialY = int(screen_height * 0.5)
 
-    while running:
-        # handle events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            # handle keypresses
-            elif event.type == pygame.KEYDOWN:
-                if pygame.key.get_pressed()[K_EQUALS]:
-                    # if '+' is pressed increase BPM
-                    bpm_count += 1
-                    _BPM_count = int((bpm_count / 60) * 200)
-                elif pygame.key.get_pressed()[K_MINUS]:
-                    # if '-' is pressed descrease BPM
-                    if bpm_count > 1:
-                        bpm_count -= 1
-                        _BPM_count = int((bpm_count / 60) * 200)
+	# graphing variables
+	posX = 0
+	index = 0
 
-        # update screen at constant framerate
-        if time.perf_counter() > previous_time + (1/30):
-            previous_time = time.perf_counter()
+	# loop variables
+	running = True
+	previous_time = 0
 
-            # put string with BPM on screen
-            bpm_string = "BPM: " + str(bpm_count)
-            bpm_text = font.render(bpm_string, False, WHITE)
-            screen.blit(bpm_text, (5, 5))
+	while running:
+		# handle events
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
 
-            # update the actual screen
-            pygame.display.update()
+		# update screen at constant framerate (60FPS)
+		if time.perf_counter() > previous_time + (1 / 60):
+			previous_time = time.perf_counter()
 
-        # update ECG at BPM interval
-        if time.perf_counter() > previous_beat + (1/_BPM_count):
-            previous_beat = time.perf_counter()
+			# clear the screen
+			screen.fill(BLACK)
 
-            # clear screen
-            screen.fill(BLACK)
+			# put string with BPM on screen
+			bpm_string = "BPM: " + str(bpm)
+			bpm_text = font.render(bpm_string, False, WHITE)
+			screen.blit(bpm_text, (5, 5))
 
-            # check if we filled the whole screen
-            if posX < screen_width:
-                # loop through all points up to current cursor position
-                for i in range(posX):
-                    # get the relevant Y positions
-                    posY1 = 100 - (ecg[index + i] * 25)  # center the signal around the middle of the screen
-                    posY2 = 100 - (ecg[index + i - 1] * 25)  # previous point
-                    # draw a line from previous point to new point
-                    pygame.draw.line(screen, GREEN, (i-1, posY2), (i, posY1), 2)
+			# increase the x-position every frame
+			if posX < screen_width:
+				posX += 1
+			else:
+				posX = 0
+				# increase the index of the array
+				if index < len(ecg) - screen_width:
+					index += screen_width
+				else:
+					# reset the index if end is reached (loop array)
+					index = 0
 
-                # calculate the latest point
-                leadPosY = 100 - (ecg[index + posX] * 25)
-                # draw the leading circle at the leading point
-                pygame.draw.circle(screen, GREEN, (posX, leadPosY), 3)
-                # increase position on X-axis for next pass
-                posX += 1
-            else:
-                # if position is off screen, reset X-coordinate
-                posX = 0
-                # check if we reached the end of the ECG data reset if nescessary
-                if (index + screen_width) > len(ecg):
-                    index = 0
-                else:
-                    index += screen_width  # pick next set from ECG array
+			# go through all x-positions
+			for i in range(posX):
+				# calculate the corresponding y-positions
+				posY = initialY - (ecg[index + i] * 100)
+				posY2 = initialY - (ecg[index + i - 1] * 100)
+
+				# draw a line between the calculated points
+				pygame.draw.line(screen, GREEN, (i, posY), (i-1, posY2), 1)
+
+				# draw a point at the head of the graph
+				if i == posX-1:
+					pygame.draw.circle(screen, GREEN, (i, posY), 2)
+
+			# update the actual screen
+			pygame.display.update()
+
 
 
 if __name__ == '__main__':
-    main()
-
+	main()
