a2aa5a25ceac11cfac5b31187a1dad99b8306d5b
emiel <e.nagel@student.utwente.nl>
2022-06-23 09:34:34 +0200

    Changed main class: it now has a loop running at 60 fps. We will call all the other functions from this loop. Put it all in self.draw().
    Also found all the sounds and have a little something for the sound class

--- Sound.py
+++ Sound.py
@@ -1,25 +1,52 @@
 """
 This class will handle the audio coming from python
 """
-
-from pydub import AudioSegment
-from pydub.playback import play
+import pygame
+from pygame import mixer
+import time
 
 
 class Sound:
 
     def __init__(self):
         "Insert stuff"
+        pygame.mixer.init()
+        self.audioSetup()
+        self.timer = 0
 
     def audioSetup(self):
         """
         This function will setup the audio
         """
+        self.selectSound = pygame.mixer.Sound('Planet select.wav')
+        self.countdownlaunchSound = pygame.mixer.Sound('Countdown launch.wav')
+        self.buzzerSound = pygame.mixer.Sound('Buzzer.wav')
+        self.heartbeepSound = pygame.mixer.Sound('Heartbeep.wav')
+        self.heartbeeplongSound = pygame.mixer.Sound('Heartbeep long.wav')
+        self.noiseSound = pygame.mixer.Sound('Space Noise.wav')
+
+        self.spaceMusic = pygame.mixer.Sound('Space music.mp3')
+        self.earthMusic = pygame.mixer.Sound('Earth music.mp3')
 
     def mixer(self):
         """
         This function will handle the sounds and when they should be played
         """
-        song = AudioSegment.from_wav("Hybrid Trap 2.wav")
-        print('playing sound using  pydub')
-        play(song)
+        self.launchSound.play()
+        time.sleep(5)
+        self.launchSound.stop()
+        time.sleep(2)
+        self.launchSound.play()
+        time.sleep(3)
+        self.spaceMusic.stop()
+        time.sleep(4)
+
+    def launchSound(self):
+
+
+        '''
+            laser = Sound()
+
+            if __name__ == '__main__':
+                laser.mixer()
+        '''
--- main.py
+++ main.py
@@ -2,48 +2,98 @@
 This class manages all the other classes in python and will handle the interaction between classes
 """
 
-from Screen import Screen
-from Sound import Sound
-from SerialController import Serial
-from DataCalculations import DatCalc
+# from Screen import Screen
+# from Sound import Sound
+# from SerialController import Serial
+# from DataCalculations import DatCalc
 import pygame
+import time
+
 
 class Main:
 
-	def __init__(self):
-		self.screen = Screen(200, 200, self.loop, self.drawLoop, title="Test window")
-		self.sound = Sound()
-		self.datCalc = DatCalc()
-		self.serial = Serial()
-		self.datCalc.dataConnect()
+    def __init__(self):
+        # self.screen = Screen(200, 200, self.loop, self.drawLoop, title="Test window")
+        # self.sound = Sound()
+        # self.datCalc = DatCalc()
+        # self.serial = Serial()
+        # self.datCalc.dataConnect()
+        pygame.init()
+        self.frameRate = 60
+        self.previous_time = time.perf_counter()
+        self.elapsed_time = 0
+        self.frameCount = 0
+        self.runBool = False
 
-	def run(self):
-		"""
-		Start functions, initializes the screen
-		"""
+    def run(self):
+        """
+        Start functions, initializes the screen
+        """
+        self.runBool = True
+        self.loop()
 
-		self.screen.start()
-		self.sound.audioSetup()
+    # self.screen.start()
+    # self.sound.audioSetup()
 
-		self.serial.serialPorts()
+    # self.serial.serialPorts()
 
-	def loop(self):
-		"""
-		Main update loop, gets run as often as possible
-		"""
-		self.serial.serialPorts()
+    def loop(self):
+        """
+        Main update loop, gets run every frame
+        """
+        while self.runBool:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or pygame.K_ESCAPE in self.get_pressed_keys():
+                    self.runBool = False
 
-	def drawLoop(self):
-		"""
-		Main draw loop, gets run at fixed interval (determined by framerate)
-		"""
+            # timing functions for constant fps
+            self.elapsed_time = time.perf_counter() - self.previous_time
+            if self.elapsed_time > 1 / self.frameRate:
+                self.previous_time = time.perf_counter()
+                # draw loop
+                self.draw()
+                self.frameCount += 1
 
-		# self.screen.loop()
-		self.sound.mixer()
+    def draw(self):
+        print(self.frameCount)
+
+    def drawLoop(self):
+        """
+        Main draw loop, gets run at fixed interval (determined by framerate)
+        """
+
+        # self.screen.loop()
+        self.sound.mixer()
+
+    def get_pressed_keys(self):
+        """
+        Function processes key presses and adds keys to an array you can check to see if a specific key is pressed
+        :returns array of pressed keys
+        """
+        """
+        Keycodes:
+        1   30      q   20      a   4       z   29
+        2   31      w   26      s   22      x   27
+        3   32      e   8       d   7       c   6
+        4   33      r   21      f   9       v   25
+        5   34      t   23      g   10      b   5
+        6   35      y   28      h   11      n   17
+        7   36      u   24      j   13      m   16
+        8   37      i   12      k   14      ,   54
+        9   38      o   18      l   15      .   55
+        0   39      p   19      ;   51      /   56
+        -   45      +   46      '   52
+        """
+        pressed_keys = pygame.key.get_pressed()
+        active_keys = set(())
+
+        for key_constant, pressed in enumerate(pressed_keys):
+            if pressed:
+                active_keys.add(key_constant)
+        return active_keys
 
 
 main = Main()
 
 if __name__ == '__main__':
-	print("Starting program")
-	main.run()
+    main.run()
