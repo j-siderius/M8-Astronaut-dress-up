79c7ac454afe8d69910e5cc3924ad2d405b2a880
emiel <e.nagel@student.utwente.nl>
2022-06-28 15:01:28 +0200

    Error message is done

--- main.py
+++ main.py
@@ -132,7 +132,10 @@ class Main:
 
         # runs every second
         if self.frameCount % 60 == 0:
-            pass
+            print(self.state)
+            print(self.preLaunched)
+            print(self.launched)
+
     # when the user is still on earth selecting planets
     def earthState(self):
         self.sound.reset()
